b9db1f240bc8b6a3545bbc2c77286ad058f0a5be
Jesper Møller Jakobsen <jespermjakobsen@gmail.com>
2020-04-26 22:25:53 +0200

    Added binary search algorithm

---
+++ BinarySearch/BinarySearch/BinarySearchAlgorithm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearch
+{
+	public static class BinarySearchAlgorithm
+	{
+		public static int FindIndexOf(this int[] array, int numberToFind)
+		{
+			if (array is null || array.Length == 0)
+				return -1;
+
+			var maxIndex = array.Length - 1;
+			var searchedIndex = maxIndex / 2;
+			return FindByIndex(array, searchedIndex, maxIndex, numberToFind);
+		}
+
+		private static int FindByIndex(int[] array, int indexToSearch, int maxIndex, int numberToFind)
+		{
+			var foundValue = array[indexToSearch];
+
+			if (foundValue == numberToFind)
+				return indexToSearch;
+
+			if(foundValue > numberToFind) //Left leg search
+				return FindByIndex(array, indexToSearch/2, indexToSearch, numberToFind);
+
+
+			//Right-leg search
+			int newIndexToSearch = (indexToSearch + maxIndex) / 2;
+			if (newIndexToSearch == indexToSearch)
+			{
+				//Dont search what we just tried. Try rounding up. Integer division cuts decimals.
+				newIndexToSearch++;
+			}
+
+			if (newIndexToSearch > maxIndex) //Index is outside of search range, which means the value is not found.
+				return -1;
+
+			return FindByIndex(array, newIndexToSearch, maxIndex, numberToFind);
+		}
+	} 
+}
---
+++ BinarySearch/BinarySearch/Tests.cs
@@ -0,0 +1,57 @@
+﻿using BinarySearch;
+using NUnit.Framework;
+using System;
+
+namespace BinarySearch
+{
+	[TestFixture]
+	class Tests
+	{	
+		[Test]
+		public void TestListOne()
+		{
+			int[] numbers = { 1, 3, 5, 7, 9, 15, 68, 99, 152 };
+
+			foreach (var number in numbers)
+			{
+				var indexOfficialMethod = Array.IndexOf(numbers, number);
+				var indexBinarySearch = numbers.FindIndexOf(number);
+
+				Assert.That(indexOfficialMethod, Is.EqualTo(indexBinarySearch));
+			}
+		}
+
+		[Test]
+		public void TestListTwo()
+		{
+			int[] numbers = { 4, 7, 9, 23, 67, 68, 165, 188, 211, 255, 655 };
+
+			foreach (var number in numbers)
+			{
+				var indexOfficialMethod = Array.IndexOf(numbers, number);
+				var indexBinarySearch = numbers.FindIndexOf(number);
+
+				Assert.That(indexOfficialMethod, Is.EqualTo(indexBinarySearch));
+			}
+		}
+
+		[Test]
+		public void TestListNotFund()
+		{
+			int[] numbers = { 4, 7, 9, 23, 67, 68, 165, 188, 211, 255, 655 };
+			int indexBinarySearch = numbers.FindIndexOf(6000);
+			int notFoundIndex = -1;
+
+			Assert.That(indexBinarySearch, Is.EqualTo(notFoundIndex));
+		}
+
+		[Test]
+		public void TestNullList()
+		{
+			int[] numbers = { 4, 7, 9, 23, 67, 68, 165, 188, 211, 255, 655 };
+			int indexBinarySearch = numbers.FindIndexOf(6000);
+			int notFoundIndex = -1;
+
+			Assert.That(indexBinarySearch, Is.EqualTo(notFoundIndex));
+		}
+}
